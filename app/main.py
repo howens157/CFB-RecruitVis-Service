2fb62693e3c0ba8af3d87f94cf35f21bda654d1f
howens157 <haydenowens52@gmail.com>
2024-08-02 23:37:53 -0400

    create recruits endpoint and add in-mem caching

--- app/main.py
+++ app/main.py
@@ -1,7 +1,14 @@
 from fastapi import FastAPI
-import cfbd
 from google.cloud import secretmanager
+from pydantic import BaseModel
+import collections
+import cfbd
 import os
+from fastapi_cache import FastAPICache
+from fastapi_cache.backends.inmemory import InMemoryBackend
+from fastapi_cache.decorator import cache
+from contextlib import asynccontextmanager
+from typing import AsyncIterator
 
 gcloudProjectId = 'weighty-psyche-431000-f6'
 gcloudSecretName = 'CFBD_api_key'
@@ -20,11 +27,46 @@ configuration.api_key['Authorization'] = CFBD_api_key
 configuration.api_key_prefix['Authorization'] = 'Bearer'
 api_config = cfbd.ApiClient(configuration)
 
-app = FastAPI()
+@asynccontextmanager
+async def lifespan(_: FastAPI) -> AsyncIterator[None]:
+    FastAPICache.init(InMemoryBackend())
+    yield
+
+app = FastAPI(lifespan=lifespan)
 
 @app.get("/teams")
-async def root():
+@cache(namespace="cfbd_calls", expire=86400)
+async def teams():
     teams_api = cfbd.TeamsApi(api_config)
     teams = teams_api.get_fbs_teams()
-    response = [{'name': school.school, 'lat': school.location.latitude, 'lng': school.location.longitude} for school in teams]
+    response = [school.school for school in teams]
     return response
+
+class RecruitsFilter(BaseModel):
+    schoolName: str
+    yearStart: int
+    yearEnd: int
+
+@cache(namespace="cfbd_calls", expire=84600)
+async def recruitsByTeamByYear(schoolName, year):
+    recruits_api = cfbd.RecruitingApi(api_config)
+    recruitsResponse = recruits_api.get_recruiting_players(year=year, team=schoolName)
+    result = []
+    for recruit in recruitsResponse:
+        if recruit.country == "USA":
+            result.append(recruit.state_province)
+    return result
+
+@app.get("/recruits")
+async def recruitsByTeam(recruitsFilter: RecruitsFilter):
+    stateCounts = collections.Counter()
+    for year in range(recruitsFilter.yearStart, recruitsFilter.yearEnd + 1):
+        currYearRecruits = await recruitsByTeamByYear(recruitsFilter.schoolName, year)
+        for state in currYearRecruits:
+            stateCounts[state] += 1
+    return [{"state_name": key, "count": stateCounts[key]} for key in stateCounts]
+
+@app.get("/clearCache")
+async def clearCache():
+    return await FastAPICache.clear(namespace="cfbd_calls")
+    
