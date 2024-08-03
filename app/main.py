0c72c4be50db9185de44e641e886ff03b5ebc662
howens157 <haydenowens52@gmail.com>
2024-08-02 22:08:58 -0400

    correct project id and secret name

--- app/main.py
+++ app/main.py
@@ -3,18 +3,19 @@ import cfbd
 from google.cloud import secretmanager
 import os
 
+gcloudProjectId = 'weighty-psyche-431000-f6'
+gcloudSecretName = 'CFBD_api_key'
+
 def get_api_key():
     envApiKey = os.environ.get("CFBD_API_KEY")
-    print(f'envApiKey {envApiKey}')
     if envApiKey: return envApiKey
     client = secretmanager.SecretManagerServiceClient()
-    secret_name = "projects/your-project-id/secrets/your-secret-name/versions/latest"
+    secret_name = f'projects/{gcloudProjectId}/secrets/{gcloudSecretName}/versions/latest'
     response = client.access_secret_version(name=secret_name)
     return response.payload.data.decode('UTF-8')
 
 configuration = cfbd.Configuration()
 CFBD_api_key = get_api_key()
-print(CFBD_api_key)
 configuration.api_key['Authorization'] = CFBD_api_key
 configuration.api_key_prefix['Authorization'] = 'Bearer'
 api_config = cfbd.ApiClient(configuration)
